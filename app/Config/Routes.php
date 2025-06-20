1ff967a37dc719dea85d387f435f946e83cc1d92
Perina185 <perinahaaaa@gmail.com>
2025-06-20 11:26:19 +0700

    push

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/','Home::index');
+$routes->get('/about', 'About::index');
 $routes->get('/ belajar', 'Belajar::index');
 $routes->get('/ percabangan', 'Percabangan::index');
 $routes->get('/ perulangan', 'Perulangan::index');
+$routes->set404Override(function() {
+        echo view('errors/not_found');
+});
